525557318eddb8e2e3dddbbb802ffacc949a0a5d
KirstyHunter <kirst2k@hotmail.com>
2013-10-27 03:35:01 +0000

    Added Dilbert stuff

---
+++ DilbertFeed/DilbertFeed/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manchestergirlgeekshackmanchester2013.DilbertFeed
+{
+    public class Class1
+    {
+        string dilbertUri = "http://dilbert.com/dyn/str_strip/000000000/00000000/0000000/100000/10000/0000/200/{0}/{1}.strip.zoom.gif";
+
+        public Class1()
+        {
+        }
+
+        public Uri GetDilbert()
+        {
+            var number = RandomNumberGenerator();
+            var uri = new Uri(string.Format(dilbertUri, number, number));
+            return uri;
+        }
+
+        private int RandomNumberGenerator()
+        {
+            var random = new Random();
+            return random.Next(110220, 110223);
+        }
+    }
+}
